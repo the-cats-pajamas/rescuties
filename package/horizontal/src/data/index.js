6ab27cf6be785e7ce4a77337d53ce4a282535dba
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-02-08 15:06:53 -0700

    removed excess files

--- package/horizontal/src/data/index.js
+++
@@ -1,6 +0,0 @@
-import mock from "./mock";
-import "./chat/ChatData";  
-import "./todo/TodoData";
-import "./notes/NotesData";
-import "./contacts/ContactsData";
-mock.onAny().passThrough();
--- package/main/src/firebase/index.js
+++
@@ -1,5 +0,0 @@
-import * as Auth from "./Auth";
-import * as Firebase from "./Firebase";
-import * as Db from "./Db";
-
-export { Auth, Db, Firebase };
