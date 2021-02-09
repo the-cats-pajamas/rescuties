5eb91687f4ffc09ccfb1806318b6a184481c96a7
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-02-09 16:32:35 -0700

    working on tokens

--- src/redux/settings/reducers/animal-reducer.js
+++ src/redux/settings/reducers/animal-reducer.js
@@ -1,21 +1,59 @@
 import superagent from 'superagent';
+require('dotenv').config();
+const id = process.env.REACT_APP_ID;
+const secret = process.env.REACT_APP_SECRET;
 
 let initialState = {
   animals: [],
 };
 
-let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJkeWJMdkVsb0JBczBrZmxTek9obXR5MXE2WVA0cllhbWVyZFNIRW8xdzBiSXkzSjY4RSIsImp0aSI6IjQ2ZTNlMTc4Zjg4NTFlZmY2ZjgwMDBlZGQ1OGJkODliOGY1YjQ0OWMzY2E0MGJjYTdlNDFiZGM0MjBhNzdiMDAwODJiMzE0ZGVkZGY2ODMyIiwiaWF0IjoxNjEyOTA3NDY2LCJuYmYiOjE2MTI5MDc0NjYsImV4cCI6MTYxMjkxMTA2Niwic3ViIjoiIiwic2NvcGVzIjpbXX0.x-ViS8Mit0_K9upP1Q2VN5zc4BvzarrSD3LeyVeC_oTjGPpMKTw5f7ha7SR40kiF7GumTq6v_ODqKum_0-v50Oe21NsRFwI_TRpaW1ZcAXgv_LJ5XNOqBuMI4c1m1yNubifnwOHSJIGn0VlIyQl_N2ssO7xjXCIyUCKXmsGDp4dE6Fb7_m4WeM-Qfs_ERQHHcBvOdIr7ay8nnw9OghCpGPbknywPg_XQJg5v2UH9s9Lz1nxx3z9tLelxAOk6llKVgiLmsYdes8mzH9i-O2StrnbHP3yS9p9QYDrOH9NEbU1H_NJ-H-PkeemZUUgbVYgTZQLRcnsb0OUPlGRF_5roxA';
+let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJkeWJMdkVsb0JBczBrZmxTek9obXR5MXE2WVA0cllhbWVyZFNIRW8xdzBiSXkzSjY4RSIsImp0aSI6IjNiZjUzMzcyODY0OTViMGJmNTFmNjdkYTc5MDQ4MmI0ZjA4ZGE3OWY0NzFmYjFmN2RkZWE0MjlmZGEyZTJjNmU5YmExOTM4OTkwYjI3ZDE4IiwiaWF0IjoxNjEyOTEyNjU5LCJuYmYiOjE2MTI5MTI2NTksImV4cCI6MTYxMjkxNjI1OSwic3ViIjoiIiwic2NvcGVzIjpbXX0.YvQ5WHHeIbRQh7TEGczaykBBV4Dyexfmuu4nnAhHHO6gYWv3g885StiLYjV-J4B5rztLHX6xo6qEILcEXqRrpsUoJgeP1EvqX3UHx8ROcVawCCDOouRTwvtBduYinwyoCQ7fIu1sQYyQLiAByxVC2zZeoY6xunHdNz5cf4RjYPaM_2D93kUjS_LmcYGeD3nvlDqXLb6Wg1-unTN1hvxF7bItxFiTLeOPStoREsV-AoCada7EZSdTMTLiDmDc8ZJ4N7G_nMM9yb9mzdb0n3O_o6dqSbLKSDsBUEB4kbHjpfccuD_8r_SiymX0VH3FF-KWTg_YGuTm6bfgcBo-Uvc1fg';
 
-const parseJwt = (token) => {
+function validateToken(token){
   try {
+    if(!token) getNewToken();
    let jwt = JSON.parse(atob(token.split('.')[1]));
-   console.log('jwt expires at', new Date(jwt.exp))
-   return jwt;
+   console.log('jwt expires at', new Date(jwt.exp));
+   if(jwt.exp > Date.now()) {
+    console.log('expired token'); 
+    getNewToken();
+   }
+   if(jwt.exp < Date.now()) {
+    console.log('valid token'); 
+     return "less than";
+   }
+   //return jwt;
   } catch (e) {
     return null;
   }
-};
-parseJwt(token);
+}
+
+function getNewToken(){
+  try{
+    superagent.post('https://api.petfinder.com/v2/oauth2/token')
+    .send({ grant_type: 'client_credentials' })
+    .send({ client_id: id })
+    .send({ client_securet: secret })
+    .then(results => {
+      let newToken = results.body.access_token;
+      console.log('new token, who dis?', newToken);
+      return newToken;
+    })
+  }
+  catch(error){
+    console.error(error)
+  };
+}
+
+setToken();
+
+async function setToken() {
+  try {
+    let token = await validateToken();
+    localStorage.setItem('token', JSON.stringify(token));
+  } catch (err) { return undefined; }
+}
+
 
 export const get = () => dispatch => {
   return superagent
@@ -27,7 +65,6 @@ export const get = () => dispatch => {
 };
 
 export const getAction = payload => {
-  console.log('payload______', payload.animals)
   return {
     type: 'GET',
     payload: payload,
@@ -35,11 +72,9 @@ export const getAction = payload => {
 };
 
 export default (state = initialState, action) => {
-  console.log('in the switch statement', action);
   let { type, payload } = action;
   switch (type) {
     case 'GET':
-      console.log('Response from get request:', payload.animals)
       return {
         results:payload,
       };
