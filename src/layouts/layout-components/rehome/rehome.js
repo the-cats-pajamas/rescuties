cbc25d7a7bfe187a74dedd343f56012286d1c394
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-02-09 20:49:08 -0700

    form setup complete

--- src/layouts/layout-components/rehome/rehome.js
+++ src/layouts/layout-components/rehome/rehome.js
@@ -9,7 +9,6 @@ import {
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
-import Step4 from './Step4';
 
 class formSteps extends Component {
 	constructor(props) {
@@ -18,7 +17,7 @@ class formSteps extends Component {
 
 		this.sampleStore = {
 			email: '',
-			gender: '',
+			name: '',
 			savedToCloud: false
 		};
 	}
@@ -38,16 +37,14 @@ class formSteps extends Component {
 		const steps =
 			[
 				{ name: 'Welcome', component: <Step1 getStore={() => (this.getStore())} updateStore={(u) => { this.updateStore(u) }} /> },
-				{ name: 'Personal Details', component: <Step2 getStore={() => (this.getStore())} updateStore={(u) => { this.updateStore(u) }} /> },
-				{ name: 'Physical Details', component: <Step3 getStore={() => (this.getStore())} updateStore={(u) => { this.updateStore(u) }} /> },
-				{ name: 'Done', component: <Step4 getStore={() => (this.getStore())} updateStore={(u) => { this.updateStore(u) }} /> }
+				{ name: 'About Your Pet', component: <Step2 getStore={() => (this.getStore())} updateStore={(u) => { this.updateStore(u) }} /> },
+				{ name: 'Done', component: <Step3 getStore={() => (this.getStore())} updateStore={(u) => { this.updateStore(u) }} /> }
 			]
 
 		return (
 			<Card>
 				<CardBody className="border-bottom">
-					<CardTitle className="mb-0"><i className="mdi mdi-border-right mr-2"></i>Form Steps</CardTitle>
-					<h6 className="card-subtitle mb-0 mt-1">Visit for more info https://www.npmjs.com/package/react-stepzilla</h6>
+					<CardTitle className="mb-0"><i className="mdi mdi-paw mr-2"></i>Rehome A Pet</CardTitle>
 				</CardBody>
 				<CardBody>
 					<div className='example'>
